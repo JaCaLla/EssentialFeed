41221cadf2d5b87b3f964ba7d3edb3eabed57f03
Javier Calatrava <javi.calatrava@gmail.com>
2025-12-12 15:33:38 +0100

    Guarantee we do not deliver a result )invoke the completion closure) after the 'RemoteFeedLoader' instance has been deallocated

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -23,15 +23,12 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { result in
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
             switch result {
             case let .success(data, response):
-                do {
-                  //  let items = try FeedItemsMapper.map(data, from: response)
-                    completion(FeedItemsMapper.map(data, from: response))
-                } catch {
-                    completion(.failure(.invalidData))
-                }
+                completion(FeedItemsMapper.map(data, from: response))
             case .failure:
                 completion(.failure(.connectivity))
             }
