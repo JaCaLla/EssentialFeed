a425928493d41af8ff3a0dae149f43aa8d0ed91a
Javier Calatrava <javi.calatrava@gmail.com>
2025-12-12 10:59:53 +0100

    Replace hardcoded value by adding a variable giving extra context

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -47,29 +47,32 @@ public final class RemoteFeedLoader {
 }
 
 private class FeedItemsMapper {
+    
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+    
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var feedItem: FeedItem {
+            return FeedItem(
+                id: id,
+                description: description,
+                location: location,
+                imageURL: image
+            )
+        }
+    }
+    
+    static var OK_200: Int { 200 }
 
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        
-        struct Root: Decodable {
-            let items: [Item]
-        }
-        
-        struct Item: Decodable {
-            let id: UUID
-            let description: String?
-            let location: String?
-            let image: URL
-            
-            var feedItem: FeedItem {
-                return FeedItem(
-                    id: id,
-                    description: description,
-                    location: location,
-                    imageURL: image
-                )
-            }
-        }
-        guard response.statusCode == 200 else {
+         
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         let root = try JSONDecoder().decode(Root.self, from: data)
