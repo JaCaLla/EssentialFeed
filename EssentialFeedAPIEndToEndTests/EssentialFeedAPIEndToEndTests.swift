7085eb16dd04c51be1e0facb9589d83683c3b48e
Javier Calatrava <javi.calatrava@gmail.com>
2025-12-21 19:45:27 +0100

    Add memory leak tracking for components in integration

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -32,10 +32,12 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         }
     }
     
-    func getFeedResult() -> LoadFeedResult? {
+    func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let client = URLSessionHTTPClient()
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "Wait for load completion")
         var receivedResult: LoadFeedResult?
         loader.load { result in
