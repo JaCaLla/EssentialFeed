1f02636345fe67f33e37b20753ab25d1ffe0994d
Javier Calatrava <javi.calatrava@gmail.com>
2025-12-21 19:37:14 +0100

    Add end-to-end API test in a separate test target

--- EssentialFeed/FeedFeature/FeedItem.swift
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -9,6 +9,13 @@ public struct FeedItem: Equatable {
 	let description: String?
 	let location: String?
 	let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
 
 
