0cd2e4eb0c4318013cfbd03c69b741ebda45c1a7
Javier Calatrava <javi.calatrava@gmail.com>
2025-12-28 09:52:00 +0100

    Remove reference of itmes in favor of images whish is a domain term used by domain experts

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -43,9 +43,9 @@ public final class RemoteFeedLoader: FeedLoader {
 }
 
 private extension Array where Element == RemoteFeedItem {
-    func toModel() -> [FeedItem] {
+    func toModel() -> [FeedImage] {
         return map {
-            FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)
+            FeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)
         }
     }
 }
--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -18,29 +18,29 @@ public final class LocalFeedLoader {
         self.currentDate = currentDate
     }
 
-    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
+    public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] cacheDeletionError in
             guard let self else { return }
             if let cacheDeletionError {
                 completion(cacheDeletionError)
             } else {
-                self.cache(items, completion: completion)
+                self.cache(feed, completion: completion)
             }
         }
     }
     
-    private func cache(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
-        store.insert(items.toLocal(), timestamp: self.currentDate(), completion: { [weak self] error in
+    private func cache(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
+        store.insert(feed.toLocal(), timestamp: self.currentDate(), completion: { [weak self] error in
             guard self != nil else { return }
             completion(error)
         } )
     }
 }
 
-private extension Array where Element == FeedItem {
-    func toLocal() -> [LocalFeedItem] {
+private extension Array where Element == FeedImage {
+    func toLocal() -> [LocalFeedImage] {
         self.map {
-            LocalFeedItem(id: $0.id,
+            LocalFeedImage(id: $0.id,
                           description: $0.description,
                           location: $0.location,
                           imageURL: $0.imageURL)
