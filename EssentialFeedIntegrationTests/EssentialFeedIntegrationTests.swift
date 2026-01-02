d5a0612eb58b8bc5f6de39f94cf90cefd52b4af6
Javier Calatrava <javi.calatrava@gmail.com>
2026-01-02 11:13:37 +0100

    Add EssentialFeedIntegrationTest target to separate the potentially slower cache integration test from the fast unit isolated tests

---
+++ EssentialFeedIntegrationTests/EssentialFeedIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedIntegrationTests.swift
+//  EssentialFeedIntegrationTests
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 2/1/26.
+//
+
+import XCTest
+
+final class EssentialFeedIntegrationTests: XCTestCase {
+
+}
