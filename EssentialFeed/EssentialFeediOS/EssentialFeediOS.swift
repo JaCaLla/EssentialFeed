079dba38a2f0509a9e27fe46922f0a5675a21a3e
Javier Calatrava <javi.calatrava@gmail.com>
2026-01-09 14:22:15 +0100

    Add EssentialFeediOS framework (prd and test) targets for iOS platform-specific components

---
+++ EssentialFeed/EssentialFeediOS/EssentialFeediOS.swift
@@ -0,0 +1,10 @@
+//
+//  EssentialFeediOS.swift
+//  EssentialFeediOS
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 9/1/26.
+//  Copyright © 2026 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
