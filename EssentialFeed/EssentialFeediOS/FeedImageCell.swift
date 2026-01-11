9f59114e31286bef79b94450272502e4d9f2587e
Javier Calatrava <javi.calatrava@gmail.com>
2026-01-11 09:03:02 +0100

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 10/1/26.
+//  Copyright © 2026 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+}
