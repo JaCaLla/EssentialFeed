d218fd2da6428c5cf0bb53aaadd0fef7a008e191
Javier Calatrava <javi.calatrava@gmail.com>
2026-01-20 21:03:54 +0100

    Replace closure event with delegate protocl to demonstrate different composition schemes

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -8,19 +8,23 @@
 
 
 import UIKit
-//import EssentialFeed
+
+protocol FeedRefreshViewControllerDelegate {
+    func didRequestFeedRefresh()
+}
 
 final class FeedRefreshViewController: NSObject, FeedLoadingView {
         
     private(set) lazy var view: UIRefreshControl = loadView()
     
-    private let loadFeed: () -> Void
-    init(loadFeed: @escaping () -> Void) {
-        self.loadFeed = loadFeed
+    private let delegate: FeedRefreshViewControllerDelegate
+    
+    init(delegate: FeedRefreshViewControllerDelegate) {
+        self.delegate = delegate
     }
     
     @objc public func refresh() {
-        loadFeed()
+        delegate.didRequestFeedRefresh()
     }
     
     fileprivate func loadView() -> UIRefreshControl {
