41fd6738052d8d718cf68d374612e7c0636f5da5
Javier Calatrava <javi.calatrava@gmail.com>
2026-01-23 18:37:12 +0100

    Create test helper to find missing localized string

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -13,11 +13,8 @@ final class FeedViewControllerTests: XCTestCase {
         let (sut, _) = makeSUT()
         sut.loadViewIfNeeded()
         
-        let bundle = Bundle(for: FeedViewController.self)
-        let localizedkey = "FEED_VIEW_TITLE"
-        let localizedTitle = bundle.localizedString(forKey: "FEED_VIEW_TITLE", value: nil, table: "Feed")
-        XCTAssertNotEqual(localizedkey, localizedTitle)
-        XCTAssertEqual(sut.title, localizedTitle)
+
+        XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
     }
 	
 	func test_loadFeedActions_requestFeedFromLoader() {
---
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewControllerTests+Localization.swift
@@ -0,0 +1,22 @@
+//
+//  FeedViewControllerTests+Localization.swift
+//  EssentialFeediOSTests
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 23/1/26.
+//  Copyright © 2026 Essential Developer. All rights reserved.
+//
+import Foundation
+import XCTest
+import EssentialFeediOS
+
+extension FeedViewControllerTests {
+    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file:file, line:line)
+        }
+        return value
+    }
+}
